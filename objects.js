ad281f1f1f7e4f6a2edb87111d84499dc8c4edee
Lydia Thornton <lydia.d.thornton@gmail.com>
2018-08-12 20:44:27 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = {Phil Ochs: "Pendulum"};
+var playlist = {[Phil Ochs]: "Pendulum"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, {[artistName]: songTitle});
