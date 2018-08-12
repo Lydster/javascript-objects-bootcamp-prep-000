5f21bf3ccf0fd0aa1da0afab7195a31d19c82551
Lydia Thornton <lydia.d.thornton@gmail.com>
2018-08-12 20:40:35 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 var playlist = {Broadcast: "Pendulum"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return object.assign(playlist, {[artistName]: songTitle});
+  object.assign({}, playlist, {[artistName]: songTitle});
+  return playlist
 }
